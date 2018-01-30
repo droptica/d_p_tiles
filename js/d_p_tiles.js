5fd2923b0d03e07dd1a55358c28136c3aa971d1e
grzegorz-pietrzak-droptica <grzegorz.pietrzak@droptica.pl>
2018-01-30 20:40:48 +0100

    Added hover effect.

--- js/d_p_tiles.js
+++ js/d_p_tiles.js
@@ -5,10 +5,33 @@
   Drupal.behaviors.droopler_subtheme = {
     attach: function (context, settings) {
       // Enable Masonry.
-      $('.d-tiles').masonry({
-        itemSelector: '.d-tiles-item',
-        columnWidth: '.d-tiles-sizer',
-        percentPosition: true,
+      $(document).ready(function() {
+        var $grid = $('.d-tiles').masonry({
+          itemSelector: '.d-tiles-item',
+          columnWidth: '.d-tiles-sizer',
+          percentPosition: true,
+        });
+
+        $(window).on('load', function(){
+          $grid.masonry('layout');
+        });
+
+
+        // Add titles to items.
+        $('.d-tiles .d-tiles-item').each(function(){
+          var alt = $(this).find('img').attr('alt');
+          var subalt = '';
+          var parts = alt.split('/', 2);
+          if (parts.length > 1) {
+            alt = parts[0];
+            subalt = parts[1];
+          }
+          var $caption_wrapper = $('<div class="d-tiles-caption"></div>').appendTo($(this));
+          var $caption = $('<div></div>').appendTo($caption_wrapper).text(alt);
+          if (subalt !== '') {
+            $('<small></small>').appendTo($caption).text(subalt);
+          }
+        });
       });
     }
   };
